11b0279c72122b35ee966f8af646d9a7fc1cda0d
SuryatejPonnapalli <148959141+SuryatejPonnapalli@users.noreply.github.com>
2024-02-15 19:37:03 +0530

    Fetch API working (Still has an issue)

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -3,34 +3,45 @@ import Navbar from './Navbar'
 import Home from './Home'
 
 function App() {
-  useEffect(() => {
-    fetchData();
- },[]);
-
- const fetchData = async () => {
-     await fetch(`${api.base}=${search}&appid=${api.key}&units=metric`)
-     .then((res)=>res.json())
-     .then((data) => {
-         setData(data)
-     })
-     setClick(prev => !prev)
- }
+    const [data, setData] = useState(null);
+    const [click, setClick] = useState(false);
+    const [search,setSearch] = useState("Hyderabad")
+    
+    const api ={
+      key: "ec3042a3092a57e5850206a04c93630e",
+      base: "https://api.openweathermap.org/data/2.5/weather?q",
+  }
   const [isDarkMode,useIsDarkMode] = useState(true)
   const darkMode = () =>{
     useIsDarkMode(prev => !prev)
   }
-    const [data, setData] = useState(null);
-    const [click, setClick] = useState(false);
+  const Search = (e) =>{
+    setSearch(e.target.value)
+  }
+  const handleClickEvent = () => {
+    setClick(prev => !prev)
+  }
+  const fetchData = async (search) => {
+    try {
+      const response = await fetch(`${api.base}=${search}&appid=${api.key}&units=metric`);
+      const data = await response.json();
+      setData(data);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
+  
+  
+  useEffect(() => {
+    fetchData(search);
+    console.log(data)
+  }, [click]);
 
-  const api ={
-    key: "ec3042a3092a57e5850206a04c93630e",
-    base: "https://api.openweathermap.org/data/2.5/weather?q",
-}
-
+ 
   return (
    <div className={`pb-1 ${isDarkMode?"bg-gradient-to-r from-[#424242] to-[#1f1f1f]":"bg-gradient-to-r from-white to-[#424242]"}`}>
-      <Navbar darkMode = {darkMode} isDarkMode={isDarkMode} fetchData={fetchData} setSearch={setSearch}/>
-      <Home isDarkMode={isDarkMode} />
+      <Navbar darkMode = {darkMode} isDarkMode={isDarkMode} fetchData={fetchData} search={Search} handleClickEvent={handleClickEvent}/>
+      <Home isDarkMode={isDarkMode} search={search} />
    </div>
   )
 }
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -3,15 +3,15 @@ import FiveDay from "./FiveDay";
 import HourlyWeather from "./HourlyWeather";
 import Weather from "./Weather";
 
-const Home = ({isDarkMode,api}) => {
+const Home = ({isDarkMode,search}) => {
     
 
     return ( 
         <div className="flex flex-col items-center justify-center">
-           <DateDiv isDarkMode={isDarkMode} api={api}/> 
-           <Weather isDarkMode={isDarkMode} api={api}/>
-           <FiveDay isDarkMode={isDarkMode} api={api}/>
-           <HourlyWeather isDarkMode={isDarkMode} api={api}/>
+           <DateDiv isDarkMode={isDarkMode} search={search}/> 
+           <Weather isDarkMode={isDarkMode} />
+           <FiveDay isDarkMode={isDarkMode} />
+           <HourlyWeather isDarkMode={isDarkMode} />
         </div>
      );
 }
